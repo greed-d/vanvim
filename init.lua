282204ef97f06131d9d5f14969b43b8b76dcdd1e
greeid <deekshit.bti@gmail.com>
2024-06-20 23:05:24 +0545

    `init` : initial commit

---
+++ init.lua
@@ -0,0 +1,14 @@
+vim.g.base46_cache = vim.fn.stdpath('data') .. '/base46_cache/'
+
+require("greed.core")
+require("greed.lazy")
+
+dofile(vim.g.base46_cache .. "defaults")
+--dofile(vim.g.base46_cache .. "statusline")
+--dofile(vim.g.base46_cache .. "git")
+--dofile(vim.g.base46_cache .. "lsp")
+--dofile(vim.g.base46_cache .. "nvdash")
+--dofile(vim.g.base46_cache .. "tbline")
+--dofile(vim.g.base46_cache .. "treesitter")
+--dofile(vim.g.base46_cache .. "telescope")
+--dofile(vim.g.base46_cache .. "whichkey")
